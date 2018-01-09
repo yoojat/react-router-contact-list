e5dd861e2a358d510053a9b6e2e243fd30aa6d5a
yoojat <yooja.ty@gmail.com>
2018-01-09 12:53:34 +0900

    Created Home page + Contacts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 
-const App = () => null;
+const App = () => (
+  <div>
+    <Header />
+  </div>
+);
 
+const Header = () => (
+  <header>
+    <h1>My Contact</h1>
+    <Route exact path="/" component={Welcome} />
+    <Route path="/contacts" component={Contacts} />
+  </header>
+);
+
+const Welcome = () => <h1>Welcome to the best contacts app!</h1>;
+
+const Contacts = () => (
+  <ul>
+    <li>Lynn</li>
+    <li>Jisu Kimchi</li>
+    <li>Japan Guy</li>
+  </ul>
+);
 export default App;
